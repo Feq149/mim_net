c48b9c49b5b416e355ba7df54f94722c02619d7c
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2025-06-16 18:08:52 +0200

    More plots

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
-.PHONY: train env test sync copy
-env:
-	. .venv/bin/activate && python3.11 -m pip install -r requirements.txt && export PYTHONPATH=.
+.PHONY: train test sync copy
 train:
-	python3.11 scripts/train_mimnet.py
+	export PYTHONPATH=. && python3.11 scripts/train_mimnet.py
 test:
-	python3.11 scripts/evaluate_mimnet.py
+	export PYTHONPATH=. && python3.11 scripts/evaluate_mimnet.py
 plot:
-	python3.11 scripts/coords.py
+	export PYTHONPATH=. && python3.11 scripts/coords.py
 sync:
 	mkdir -p checkpoints logs
 	scp miracle:~/mim_net/checkpoints/*.pth checkpoints/ && scp -r miracle:~/mim_net/logs/* logs/
--- scripts/coords.py
+++ scripts/coords.py
@@ -5,10 +5,10 @@ from scripts.visualize_3d import plot_real_vs_predicted
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dataloaders = scn.load(casp_version=7, with_pytorch="dataloaders", batch_size=1)
-test_loader = dataloaders["train"]  # type: ignore
+test_loader = dataloaders["test"]  # type: ignore
 
 model = MimNet(in_channels=40, nf=128, T=6, n_levels=3).to(device)
-checkpoint_path = "checkpoints/mimnet_model.pth"
+checkpoint_path = "checkpoints/mimnet_model_without_reg.pth"
 model.load_state_dict(torch.load(checkpoint_path, map_location=device))
 
 # Save images to files instead of displaying
